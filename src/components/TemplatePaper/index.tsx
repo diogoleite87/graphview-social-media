d607d01bde094eaf77b50ea80ff8492aa9caec56
Diogo Leite <diogoleite87@gmail.com>
2023-07-24 16:03:11 -0300

    feat: add template paper

---
+++ src/components/TemplatePaper/index.tsx
@@ -0,0 +1,51 @@
+import { Accordion, AccordionDetails, AccordionSummary, Box, Button, Paper, Stack, Typography } from "@mui/material"
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+import { GraphData, GraphTemplate } from "../../schemas"
+
+export interface ITemplateCard {
+    data: GraphTemplate,
+    setDataGraph(state: GraphData): void,
+    setState(state: boolean): void
+}
+
+export default function TemplatePaper({ data, setDataGraph, setState }: ITemplateCard) {
+
+    const handleSubmit = () => {
+        setState(false)
+        setDataGraph(data.graphData)
+    }
+
+    return (
+        <Paper elevation={1} sx={{ p: 1 }}>
+
+            <Stack spacing={2} sx={{ alignItems: "center", justifyContent: 'space-between', margin: '2vh' }} direction="row">
+                <Box>
+                    <Typography variant="h5" >{data.title}</Typography>
+                    <Typography variant="subtitle1" >Data: {data.createdAt}</Typography>
+                    <Typography variant="subtitle1" >Créditos: {data.credits}</Typography>
+                    <Typography variant="subtitle1" >Número de Nós: {data.graphData.nodes.length}</Typography>
+                    <Typography variant="subtitle1" >Número de Arestas: {data.graphData.links.length}</Typography>
+                </Box>
+                <Box>
+                    <Button variant="contained" component="label" color="primary" onClick={handleSubmit} endIcon={<UploadFileIcon />} >Povoar</Button>
+                </Box>
+            </Stack>
+
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>Informações do Grafo</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        {data.body}
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        </Paper>
+    )
+}
